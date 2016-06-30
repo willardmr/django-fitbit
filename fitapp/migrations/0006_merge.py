869ba49c980f017436bed6c29a6b3759d0733cdd
Max Willard <maxwell.rubin.willard@gmail.com>
2016-06-30 10:08:43 -0500

    Added merged migration file.

---
+++ fitapp/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('fitapp', '0004_auto_20151204_1654'),
+        ('fitapp', '0005_upgrade_oauth1_tokens_to_oauth2'),
+    ]
+
+    operations = [
+    ]
